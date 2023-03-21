93e12d2d6cd4d974827b121209ad3ea18bd56617
dgoldsb <dgoldsb@live.nl>
2023-03-22 00:37:11 +0100

    All deserialization

--- app/src/main/kotlin/advent/classes/day16/OpcodeInstruction.kt
+++ app/src/main/kotlin/advent/classes/day16/OpcodeInstruction.kt
@@ -1,9 +1,18 @@
 package advent.classes.day16
 
-/**
- * Mysterious Opcode instruction, we do not know which Opcode each maps to!
- */
+/** Mysterious Opcode instruction, we do not know which Opcode each maps to! */
 data class OpcodeInstruction(val opcode: Int, val inputA: Int, val inputB: Int, val output: Int) {
+  companion object {
+    private val REGEX: Regex = Regex("(\\d+) (\\d+) (\\d+) (\\d+)")
 
+    fun fromString(input: String): OpcodeInstruction {
+      val match: MatchResult = REGEX.matchEntire(input)!!
 
+      return OpcodeInstruction(
+          match.groupValues[1].toInt(),
+          match.groupValues[2].toInt(),
+          match.groupValues[3].toInt(),
+          match.groupValues[4].toInt())
+    }
+  }
 }
--- app/src/main/kotlin/advent/days/Day16.kt
+++ app/src/main/kotlin/advent/days/Day16.kt
@@ -12,7 +12,7 @@ object Day16 : Day<Pair<List<OpcodeSample>, List<OpcodeInstruction>>>(16) {
     val joinedInput = input.joinToString("\n")
 
     // Split by double newline.
-    val splitInputPair = joinedInput.split("\n\n\n")
+    val splitInputPair = joinedInput.split("\n\n\n\n")
     require(splitInputPair.size == 2) { "Expected two components to this input!" }
 
     // Then deserialize the opcode samples.
@@ -20,13 +20,16 @@ object Day16 : Day<Pair<List<OpcodeSample>, List<OpcodeInstruction>>>(16) {
     val samples = rawSamples.map { string: String -> OpcodeSample.fromString(string) }
 
     // Finally deserialize the opcode instructions.
-    // TODO
+    val rawInstructions = splitInputPair[1].split("\n")
+    val instructions =
+        rawInstructions.map { string: String -> OpcodeInstruction.fromString(string) }
 
-    return Pair(samples, listOf())
+    return Pair(samples, instructions)
   }
 
   override fun part1(input: Pair<List<OpcodeSample>, List<OpcodeInstruction>>): String {
     val candidateOpcodes = listOf(Opcode.Addi(), Opcode.Addr())
+    // TODO: Function to get map, derive answer from here.
     for (sample in input.first) {
       val filteredOpcodes = sample.filterCandidateOpcodes(candidateOpcodes)
     }
