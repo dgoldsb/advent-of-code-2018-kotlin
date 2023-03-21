9731e0db8d23f1d22e77537c8cbc789e45ae1ac2
dgoldsb <dgoldsb@live.nl>
2023-03-22 00:33:50 +0100

    Add deserialization for first part

--- app/src/main/kotlin/advent/classes/day16/OpcodeSample.kt
+++ app/src/main/kotlin/advent/classes/day16/OpcodeSample.kt
@@ -1,5 +1,8 @@
 package advent.classes.day16
 
+import java.util.*
+import kotlin.collections.HashSet
+
 data class OpcodeSample(
     val before: RegisterState,
     val after: RegisterState,
@@ -22,5 +25,34 @@ data class OpcodeSample(
     return this.after ==
         opcode.apply(
             this.before, this.instruction.inputA, this.instruction.inputB, this.instruction.output)
+  }
+
+  companion object {
+    private val REGEX: Regex =
+        Regex(
+            "Before: \\[(\\d+), (\\d+), (\\d+), (\\d+)\\]" +
+                    "\\n(\\d+) (\\d+) (\\d+) (\\d+)" +
+                    "\\nAfter:  \\[(\\d+), (\\d+), (\\d+), (\\d+)\\]")
+
+    fun fromString(input: String): OpcodeSample {
+      val match: MatchResult = REGEX.matchEntire(input)!!
+
+      return OpcodeSample(
+          RegisterState(
+              match.groupValues[1].toInt(),
+              match.groupValues[2].toInt(),
+              match.groupValues[3].toInt(),
+              match.groupValues[4].toInt()),
+          RegisterState(
+              match.groupValues[9].toInt(),
+              match.groupValues[10].toInt(),
+              match.groupValues[11].toInt(),
+              match.groupValues[12].toInt()),
+          OpcodeInstruction(
+              match.groupValues[5].toInt(),
+              match.groupValues[6].toInt(),
+              match.groupValues[7].toInt(),
+              match.groupValues[8].toInt()))
+    }
   }
 }
--- app/src/main/kotlin/advent/days/Day16.kt
+++ app/src/main/kotlin/advent/days/Day16.kt
@@ -9,18 +9,27 @@ object Day16 : Day<Pair<List<OpcodeSample>, List<OpcodeInstruction>>>(16) {
 
   override fun deserialize(input: List<String>): Pair<List<OpcodeSample>, List<OpcodeInstruction>> {
     // First join all lines together.
+    val joinedInput = input.joinToString("\n")
 
     // Split by double newline.
+    val splitInputPair = joinedInput.split("\n\n\n")
+    require(splitInputPair.size == 2) { "Expected two components to this input!" }
 
     // Then deserialize the opcode samples.
+    val rawSamples = splitInputPair[0].split("\n\n")
+    val samples = rawSamples.map { string: String -> OpcodeSample.fromString(string) }
 
     // Finally deserialize the opcode instructions.
+    // TODO
 
-    return Pair(listOf(), listOf())
+    return Pair(samples, listOf())
   }
 
   override fun part1(input: Pair<List<OpcodeSample>, List<OpcodeInstruction>>): String {
     val candidateOpcodes = listOf(Opcode.Addi(), Opcode.Addr())
+    for (sample in input.first) {
+      val filteredOpcodes = sample.filterCandidateOpcodes(candidateOpcodes)
+    }
     return ""
   }
 
