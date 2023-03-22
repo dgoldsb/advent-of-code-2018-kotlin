fbfa5c4ef4aa6b05780457d27f747fc9d4125c43
dgoldsb <dgoldsb@live.nl>
2023-03-22 09:12:19 +0100

    Finish day 16

--- app/src/main/kotlin/advent/classes/day16/Opcode.kt
+++ app/src/main/kotlin/advent/classes/day16/Opcode.kt
@@ -3,7 +3,7 @@ package advent.classes.day16
 sealed class Opcode : OpcodeInterface {
 
   /** addr (add register) stores into register C the result of adding register A and register B. */
-  class Addr : Opcode() {
+  object Addr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -17,7 +17,7 @@ sealed class Opcode : OpcodeInterface {
   }
 
   /** addi (add immediate) stores into register C the result of adding register A and value B. */
-  class Addi : Opcode() {
+  object Addi : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -33,7 +33,7 @@ sealed class Opcode : OpcodeInterface {
    * mulr (multiply register) stores into register C the result of multiplying register A and
    * register B.
    */
-  class Mulr : Opcode() {
+  object Mulr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -50,7 +50,7 @@ sealed class Opcode : OpcodeInterface {
    * muli (multiply immediate) stores into register C the result of multiplying register A and value
    * B.
    */
-  class Muli : Opcode() {
+  object Muli : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -66,7 +66,7 @@ sealed class Opcode : OpcodeInterface {
    * banr (bitwise AND register) stores into register C the result of the bitwise AND of register A
    * and register B.
    */
-  class Banr : Opcode() {
+  object Banr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -83,7 +83,7 @@ sealed class Opcode : OpcodeInterface {
    * bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A
    * and value B.
    */
-  class Bani : Opcode() {
+  object Bani : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -99,7 +99,7 @@ sealed class Opcode : OpcodeInterface {
    * borr (bitwise OR register) stores into register C the result of the bitwise OR of register A
    * and register B.
    */
-  class Borr : Opcode() {
+  object Borr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -116,7 +116,7 @@ sealed class Opcode : OpcodeInterface {
    * bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A
    * and value B.
    */
-  class Bori : Opcode() {
+  object Bori : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -131,7 +131,7 @@ sealed class Opcode : OpcodeInterface {
   /**
    * setr (set register) copies the contents of register A into register C. (Input B is ignored.)
    */
-  class Setr : Opcode() {
+  object Setr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -144,7 +144,7 @@ sealed class Opcode : OpcodeInterface {
   }
 
   /** seti (set immediate) stores value A into register C. (Input B is ignored.) */
-  class Seti : Opcode() {
+  object Seti : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -159,19 +159,18 @@ sealed class Opcode : OpcodeInterface {
    * gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register
    * B. Otherwise, register C is set to 0.
    */
-  class Gtir : Opcode() {
+  object Gtir : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
         inputB: Int,
         output: Int
     ): RegisterState {
-      val valueA = register.get(inputA)
       val valueB = register.get(inputB)
-      if (valueA > valueB) {
-        return register.set(1, output)
+      return if (inputA > valueB) {
+        register.set(1, output)
       } else {
-        return register.set(0, output)
+        register.set(0, output)
       }
     }
   }
@@ -180,7 +179,7 @@ sealed class Opcode : OpcodeInterface {
    * gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value
    * B. Otherwise, register C is set to 0.
    */
-  class Gtri : Opcode() {
+  object Gtri : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -188,10 +187,10 @@ sealed class Opcode : OpcodeInterface {
         output: Int
     ): RegisterState {
       val valueA = register.get(inputA)
-      if (valueA > inputB) {
-        return register.set(1, output)
+      return if (valueA > inputB) {
+        register.set(1, output)
       } else {
-        return register.set(0, output)
+        register.set(0, output)
       }
     }
   }
@@ -200,26 +199,7 @@ sealed class Opcode : OpcodeInterface {
    * gtrr (greater-than register/register) sets register C to 1 if register A is greater than
    * register B. Otherwise, register C is set to 0.
    */
-  class Gtrr : Opcode() {
-    override fun apply(
-        register: RegisterState,
-        inputA: Int,
-        inputB: Int,
-        output: Int
-    ): RegisterState {
-      if (inputA > inputB) {
-        return register.set(1, output)
-      } else {
-        return register.set(0, output)
-      }
-    }
-  }
-
-  /**
-   * eqir (equal immediate/register) sets register C to 1 if value A is equal to register B.
-   * Otherwise, register C is set to 0.
-   */
-  class Eqir : Opcode() {
+  object Gtrr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -228,10 +208,30 @@ sealed class Opcode : OpcodeInterface {
     ): RegisterState {
       val valueA = register.get(inputA)
       val valueB = register.get(inputB)
-      if (valueA == valueB) {
-        return register.set(1, output)
+      return if (valueA > valueB) {
+        register.set(1, output)
       } else {
-        return register.set(0, output)
+        register.set(0, output)
+      }
+    }
+  }
+
+  /**
+   * eqir (equal immediate/register) sets register C to 1 if value A is equal to register B.
+   * Otherwise, register C is set to 0.
+   */
+  object Eqir : Opcode() {
+    override fun apply(
+        register: RegisterState,
+        inputA: Int,
+        inputB: Int,
+        output: Int
+    ): RegisterState {
+      val valueB = register.get(inputB)
+      return if (inputA == valueB) {
+        register.set(1, output)
+      } else {
+        register.set(0, output)
       }
     }
   }
@@ -240,7 +240,7 @@ sealed class Opcode : OpcodeInterface {
    * eqri (equal register/immediate) sets register C to 1 if register A is equal to value B.
    * Otherwise, register C is set to 0.
    */
-  class Eqri : Opcode() {
+  object Eqri : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
@@ -248,10 +248,10 @@ sealed class Opcode : OpcodeInterface {
         output: Int
     ): RegisterState {
       val valueA = register.get(inputA)
-      if (valueA == inputB) {
-        return register.set(1, output)
+      return if (valueA == inputB) {
+        register.set(1, output)
       } else {
-        return register.set(0, output)
+        register.set(0, output)
       }
     }
   }
@@ -260,17 +260,19 @@ sealed class Opcode : OpcodeInterface {
    * eqrr (equal register/register) sets register C to 1 if register A is equal to register B.
    * Otherwise, register C is set to 0.
    */
-  class Eqrr : Opcode() {
+  object Eqrr : Opcode() {
     override fun apply(
         register: RegisterState,
         inputA: Int,
         inputB: Int,
         output: Int
     ): RegisterState {
-      if (inputA == inputB) {
-        return register.set(1, output)
+      val valueA = register.get(inputA)
+      val valueB = register.get(inputB)
+      return if (valueA == valueB) {
+        register.set(1, output)
       } else {
-        return register.set(0, output)
+        register.set(0, output)
       }
     }
   }
--- app/src/main/kotlin/advent/days/Day16.kt
+++ app/src/main/kotlin/advent/days/Day16.kt
@@ -4,8 +4,28 @@ import advent.Day
 import advent.classes.day16.Opcode
 import advent.classes.day16.OpcodeInstruction
 import advent.classes.day16.OpcodeSample
+import advent.classes.day16.RegisterState
 
 object Day16 : Day<Pair<List<OpcodeSample>, List<OpcodeInstruction>>>(16) {
+
+  private val candidateOpcodes =
+      listOf(
+          Opcode.Addi,
+          Opcode.Addr,
+          Opcode.Mulr,
+          Opcode.Muli,
+          Opcode.Banr,
+          Opcode.Bani,
+          Opcode.Borr,
+          Opcode.Bori,
+          Opcode.Setr,
+          Opcode.Seti,
+          Opcode.Gtir,
+          Opcode.Gtri,
+          Opcode.Gtrr,
+          Opcode.Eqir,
+          Opcode.Eqri,
+          Opcode.Eqrr)
 
   override fun deserialize(input: List<String>): Pair<List<OpcodeSample>, List<OpcodeInstruction>> {
     // First join all lines together.
@@ -28,32 +48,99 @@ object Day16 : Day<Pair<List<OpcodeSample>, List<OpcodeInstruction>>>(16) {
   }
 
   override fun part1(input: Pair<List<OpcodeSample>, List<OpcodeInstruction>>): String {
-    val candidateOpcodes =
-        listOf(
-            Opcode.Addi(),
-            Opcode.Addr(),
-            Opcode.Mulr(),
-            Opcode.Muli(),
-            Opcode.Banr(),
-            Opcode.Bani(),
-            Opcode.Borr(),
-            Opcode.Bori(),
-            Opcode.Setr(),
-            Opcode.Seti(),
-            Opcode.Gtir(),
-            Opcode.Gtri(),
-            Opcode.Gtrr(),
-            Opcode.Eqir(),
-            Opcode.Eqri(),
-            Opcode.Eqrr())
-    // TODO: Function to get map, derive answer from here.
+    var count = 0
     for (sample in input.first) {
-      val filteredOpcodes = sample.filterCandidateOpcodes(candidateOpcodes)
+      if (sample.filterCandidateOpcodes(candidateOpcodes).size >= 3) {
+        count += 1
+      }
     }
-    return ""
+    return count.toString()
+  }
+
+  /** Find the possible opcodes per integer, judging from the input samples. */
+  private fun checkPossibleOpcodesPerInteger(
+      samples: List<OpcodeSample>
+  ): HashMap<Int, Set<Opcode>> {
+    val intOpcodesMap = HashMap<Int, Set<Opcode>>()
+    for (sample in samples) {
+      val currentSet =
+          intOpcodesMap.getOrDefault(sample.instruction.opcode, candidateOpcodes.toSet())
+      val candidates = sample.filterCandidateOpcodes(candidateOpcodes)
+      intOpcodesMap[sample.instruction.opcode] = currentSet.intersect(candidates)
+    }
+    return intOpcodesMap
+  }
+
+  /** We can find Opcodes by looking if any key in the `puzzledState` only has one option left. */
+  private fun eliminateByExclusion(
+      target: HashMap<Int, Opcode>,
+      puzzledState: HashMap<Int, Set<Opcode>>
+  ): Boolean {
+    for (entry in puzzledState.entries) {
+      if (entry.value.size == 1) {
+        val opcode = entry.value.first()
+
+        // Save in the target.
+        target[entry.key] = opcode
+
+        // Remove opcode from all opcode sets, as we have found our match!
+        for (otherEntry in puzzledState.entries) {
+          puzzledState[otherEntry.key] = otherEntry.value.minus(opcode)
+        }
+
+        // Remove from the puzzled state.
+        puzzledState.remove(entry.key)
+        return true
+      }
+    }
+    return false
+  }
+
+  private fun eliminateByRemaining(
+      target: HashMap<Int, Opcode>,
+      puzzledState: HashMap<Int, Set<Opcode>>
+  ): Boolean {
+    for (opcode in candidateOpcodes) {
+      var matchingInteger: Int? = null
+      var memberCount = 0
+      for (entry in puzzledState.entries) {
+        if (entry.value.contains(opcode)) {
+          memberCount += 1
+          matchingInteger = entry.key
+        }
+      }
+
+      if (memberCount == 1) {
+        puzzledState.remove(matchingInteger)
+        target[matchingInteger!!] = opcode
+        return true
+      }
+    }
+    return false
   }
 
   override fun part2(input: Pair<List<OpcodeSample>, List<OpcodeInstruction>>): String {
-    return ""
+    val intOpcodesMap = checkPossibleOpcodesPerInteger(input.first)
+
+    // Construct the final map by eliminating one each round.
+    val intOpcodeMap = HashMap<Int, Opcode>()
+    while (intOpcodesMap.isNotEmpty()) {
+      val resultExclusion = eliminateByExclusion(intOpcodeMap, intOpcodesMap)
+      val resultRemaining = eliminateByRemaining(intOpcodeMap, intOpcodesMap)
+      require(resultExclusion or resultRemaining) { "The samples are ambiguous!" }
+    }
+
+    // Run the program.
+    var register = RegisterState(0, 0, 0, 0)
+    for (opcodeInstruction in input.second) {
+      val opcode = intOpcodeMap[opcodeInstruction.opcode]!!
+      register =
+          opcode.apply(
+              register,
+              opcodeInstruction.inputA,
+              opcodeInstruction.inputB,
+              opcodeInstruction.output)
+    }
+    return register.get(0).toString()
   }
 }
